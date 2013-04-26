d8f6867b9a1636de41e10decc45e42126d632f06
Tara Thorne <tthorne@fullsail.edu>
2013-04-25 23:38:30 -0400

    Assignment - Conidtions
    Assignment - Conidtions

---
+++ Assignment- Conditionals/Conditions_Wacky/js/script.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+/* Student Name:  Tara Thorne
+ * Date: Wednesday April 25, 2013
+ * Assignment:  Assignment- Conditions (Wacky)
+ */
+
+var wacky = alert("Do I want hot chocolate or ice cream?  If it is below 40 \xB0 F I'll drink Hot Chocolate");
+var temp = prompt("What is the current temperature?");
+
+drink = (drinkADay * drink * 30) / age;
+
+console.log(drink)
