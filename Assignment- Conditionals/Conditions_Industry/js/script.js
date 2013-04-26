226df4b17652aad02e59f5253efe4afa5d4645b8
tthorne <tthorne@fullsail.edu>
2013-04-26 00:24:09 -0300

    Update script.js

--- Assignment- Conditionals/Conditions_Industry/js/script.js
+++ Assignment- Conditionals/Conditions_Industry/js/script.js
@@ -8,7 +8,7 @@ var previousMonth = prompt("What was previous month's profit?"); /* Asks for the
 var lastMonth = prompt("What was last month's profit?"); /* Asks for last month's profit amount. */
 
 profit = (previousMonth < lastMonth) ? "The store made a profit compared to last month." : "The store didn't make a profit compared to last month.";
-/* */
+/* If last month profit was greater than the previous month then the result will be "The store made a profit compared to last month." */
 
 
 console.log(profit);
