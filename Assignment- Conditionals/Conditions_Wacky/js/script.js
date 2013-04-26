68ae88fa6bb525844eb6dc643e6783f7c7a6f189
tthorne <tthorne@fullsail.edu>
2013-04-26 00:53:06 -0300

    Update script.js

--- Assignment- Conditionals/Conditions_Wacky/js/script.js
+++ Assignment- Conditionals/Conditions_Wacky/js/script.js
@@ -4,14 +4,16 @@
  * Assignment:  Assignment- Conditions (Wacky)
  */
 
-var wacky = alert("Do I want hot chocolate, Mountain Dew or ice cream?  If it is below 40 \xB0 F I'll drink Hot Chocolate");
+var wacky = alert("Do I want hot chocolate, Mountain Dew or ice cream?  If it is below 40 \xB0 F I'll drink Hot Chocolate.  If it's above 70 \xB0 I'll eat ice cream.");
 var temp = prompt("What is the current temperature?");
 
-if (temp <= 40) /* Checks to make ammonia and nitrites equal 0 and nitrates are less then 50 ppm */
+if (temp <= 40) /* If temperature is less then 40 */
 {
-	change = "I'm drinking hot chocolate."; /* Result if ammonia, nitrites are at 0 and nitrates are less then 50 ppm */
+	drink = "I'm drinking hot chocolate."; /* This is the result. */
+}else if (temp >= 41 && temp <= 70) {
+	drink = "I'm drinking Mountain Dew";
 }else{
 	change = ("I'm eating ice cream."); /* Result if ammonia, nitrites don't equal 0 and nitrates are greater then 50 ppm */
 }
 
-console.log(change);
+console.log(drink);
