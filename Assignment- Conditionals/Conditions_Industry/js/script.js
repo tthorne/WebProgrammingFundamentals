9f1d60608fe503fb7dea5e58d03b6fb19c452f25
tthorne <tthorne@fullsail.edu>
2013-04-26 00:21:21 -0300

    Update script.js

--- Assignment- Conditionals/Conditions_Industry/js/script.js
+++ Assignment- Conditionals/Conditions_Industry/js/script.js
@@ -6,13 +6,7 @@
 var item = alert ("Did the store make a profit last month compared to the previous month?"); /*  */
 var previousMonth = prompt("What was previous month's profit?"); /*  */
 var lastMonth = prompt("What was last month's profit?"); /*  */
-var thismMonth = prompt("How much has the store brought in so far?"); /*  */
 
-if (previousMonth < lastMonth) /* Checks to make ammonia and nitrites equal 0 and nitrates are less then 50 ppm */
-{
-	profit = "Store made a profit."; /* Result if ammonia, nitrites are at 0 and nitrates are less then 50 ppm */
-}else{
-	profit = "Store didn't make a profit"; /* Result if ammonia, nitrites don't equal 0 and nitrates are greater then 50 ppm */
-}
+profit = (previousMonth < lastMonth) ? "The store made a profit compared to last month." : "The store didn't make a profit compared to last month.";
 
 console.log(profit);
