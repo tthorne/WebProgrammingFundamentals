e5d9114f18f31094383ae997cf86c9ca3e084308
tthorne <tthorne@fullsail.edu>
2013-04-25 22:14:22 -0300

    Update script.js

--- Assignment- Conditionals/Conditions_Personal/js/script.js
+++ Assignment- Conditionals/Conditions_Personal/js/script.js
@@ -10,11 +10,11 @@ var ammonia = prompt("What is the ammonia level?"); /* Asks what the ammonia lev
 var nitrite = prompt("What is the nitrite level?"); /* Asks what the nitrite level of the tank is. */
 var nitrate = prompt("What is the nitrate level?"); /* Asks what the nitrate level of the tank is. */
 
-if (ammonia == 0 && nitrite == 0 && nitrate <= 50) /* */
+if (ammonia == 0 && nitrite == 0 && nitrate <= 50) /* Checks to make*/
 {
 	change = "You don't need to change the tank water."; /* Result if ammonia, nitrites are at 0 and nitrates are less then 50 ppm */
 }else{
-	change = ("You need to change the tank water");
+	change = ("You need to change the tank water"); /* Result if ammonia, nitrites don't equal 0 and nitrates are greater then 50 ppm */
 }
 
 console.log(change);
