c77700331d40f3fa7ada28136822c03790bcb8f3
tthorne <tthorne@fullsail.edu>
2013-04-26 00:22:10 -0300

    Update script.js

--- Assignment- Conditionals/Conditions_Industry/js/script.js
+++ Assignment- Conditionals/Conditions_Industry/js/script.js
@@ -3,8 +3,8 @@
  * Date: Wednesday April 25, 2013
  * Assignment:  Assignment- Conditions (Industry)
  */
-var item = alert ("Did the store make a profit last month compared to the previous month?"); /*  */
-var previousMonth = prompt("What was previous month's profit?"); /*  */
+var item = alert ("Did the store make a profit last month compared to the previous month?"); /* States what the code is for */
+var previousMonth = prompt("What was previous month's profit?"); /* Asks for the previous month profit amount. */
 var lastMonth = prompt("What was last month's profit?"); /*  */
 
 profit = (previousMonth < lastMonth) ? "The store made a profit compared to last month." : "The store didn't make a profit compared to last month.";
