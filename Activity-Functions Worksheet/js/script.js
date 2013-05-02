c6c87a8ba7ae188e74352c20d27582a699b8e16d
tthorne <tthorne@fullsail.edu>
2013-05-01 22:11:01 -0300

    Update script.js

--- Activity-Functions Worksheet/js/script.js
+++ Activity-Functions Worksheet/js/script.js
@@ -14,7 +14,7 @@ Calculate the circumference of a circle.
     Result to print to the console:  “The circumference of the circle is X”;
 */
 
-function calCircumference(r){
+function calCircumference(r){ // Starts the function
 	var r = prompt("What is the radius of the circle?"); // Asks the user for the radius of the circle.
 	var circumference = 2 * 3.14 * r; // Calculates the circumference by taking 2 x pi x the radius
 	console.log("The circumference of the circle is " + circumference); // The result of the calculations
@@ -31,3 +31,10 @@ It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many 
     Return: Number of Bee stings
     Result to print to the console: “It takes X bee stings to kill a X “
 */
+
+function beeStings(r){
+	var r = prompt("What is the radius of the circle?"); // Asks the user for the radius of the circle.
+	var circumference = 2 * 3.14 * r; // Calculates the circumference by taking 2 x pi x the radius
+	console.log("The circumference of the circle is " + circumference); // The result of the calculations
+	}
+calCircumference(); // Calls the function
