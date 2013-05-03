52f31e7e4180b584c187fe2eeca6612f1df1e464
tthorne <tthorne@fullsail.edu>
2013-05-03 01:38:18 -0300

    Update script.js

--- Assignment- Functions/Functions_Industry/js/script.js
+++ Assignment- Functions/Functions_Industry/js/script.js
@@ -6,11 +6,12 @@
 
 
 /* What was the average that people spent on instant scratch off tickets and online tickets in the store today? */
-var lotteryTickets = function(twenties, tens, fives, threes, twos, ones, online){
-	var totalTickets = (twenties + tens + fives + threes + twos + ones + online) / 7;
+var lotteryTickets = function(twenties, tens, fives, threes, twos, ones, online){ // The paramaters
+	var totalTickets = (twenties + tens + fives + threes + twos + ones + online) / 7; 
+	// Tkes the paramaters and totals up the arugments and then divides it by 7
 	return totalTickets;
 }
 
-var totalSales = lotteryTickets (130, 50, 25, 18, 20, 15, 500);
+var totalSales = lotteryTickets (130, 50, 25, 18, 20, 15, 500); // The arguments
 sales = parseInt(totalSales);
 console.log(sales);
