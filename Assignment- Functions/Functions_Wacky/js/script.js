a2f5b4f8023be07365ec26872b95ca337ad7f8ab
tthorne <tthorne@fullsail.edu>
2013-05-03 01:04:42 -0300

    Update script.js

--- Assignment- Functions/Functions_Wacky/js/script.js
+++ Assignment- Functions/Functions_Wacky/js/script.js
@@ -1,16 +1,19 @@
 // JavaScript Document
 /* Student Name:  Tara Thorne
- * Date: Wednesday May 1, 2013
- * Assignment:  Assignment- Functions (Wacky)
+ * Date: Wednesday April 25, 2013
+ * Assignment:  Assignment- Conditions (Wacky)
  */
 
+var wacky = alert("Do I want hot chocolate, Mountain Dew or ice cream?  If it is below 40 \xB0 F I'll drink Hot Chocolate.  If it's above 70 \xB0 I'll eat ice cream.");
+var temp = prompt("What is the current temperature?");
 
-/* How much money do I need to pay when I get to Pennsic War? */
-var pennsicFees = function(fee, preRegfee, memberFee){
-	var area = fee - preRegfee - memberFee;
-	return area;
-};
+if (temp <= 40) /* If temperature is less then 40 */
+{
+	drink = "I'm drinking hot chocolate."; /* This is the result. */
+}else if (temp >= 41 && temp <= 70) { /* If temperature is greater than 41 and less than 70 I'll drink Mountain Dew. */
+	drink = "I'm drinking Mountain Dew";
+}else{
+	drink = ("I'm eating ice cream."); /* If temperature is greater than 71 I'll eat ice cream. */
+}
 
-var a = calArea(165, 10, 25);
-
-console.log(a);
+console.log(drink);
