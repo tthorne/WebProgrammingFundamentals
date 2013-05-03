fb637592d72cfe66ab655d1f4ff134a1c38e04d2
Tara Thorne <tthorne@fullsail.edu>
2013-05-02 23:37:44 -0400

    Assignment- Functions
    Assignment- Functions

---
+++ Assignment- Functions/Functions_Wacky/js/script.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+/* Student Name:  Tara Thorne
+ * Date: Wednesday May 1, 2013
+ * Assignment:  Assignment- Functions (Wacky)
+ */
+
+
+/* How much money do I need to pay when I get to Pennsic War? */
+var pennsicFees = function(fee, preRegfee, memberFee){
+	var area = fee - preRegfee - memberFee;
+	return area;
+};
+
+var a = calArea(165, 10, 25);
+
+console.log(a);
