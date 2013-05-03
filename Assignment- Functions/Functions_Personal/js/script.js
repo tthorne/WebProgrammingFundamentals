666d84e375e5885e869f6bfae4b9c13009ba6640
tthorne <tthorne@fullsail.edu>
2013-05-02 23:13:24 -0300

    Update script.js

--- Assignment- Functions/Functions_Personal/js/script.js
+++ Assignment- Functions/Functions_Personal/js/script.js
@@ -4,17 +4,13 @@
  * Assignment:  Assignment- Functions (Personal)
  */
 
-/* Should I change my fish tank water? */
-var personal = alert("Do you need to change your tank water?");
-var ammonia = prompt("What is the ammonia level?"); /* Asks what the ammonia level of the tank is. */
-var nitrite = prompt("What is the nitrite level?"); /* Asks what the nitrite level of the tank is. */
-var nitrate = prompt("What is the nitrate level?"); /* Asks what the nitrate level of the tank is. */
 
-if (ammonia == 0 && nitrite == 0 && nitrate <= 50) /* Checks to make ammonia and nitrites equal 0 and nitrates are less then 50 ppm */
-{
-	change = "You don't need to change the tank water."; /* Result if ammonia, nitrites are at 0 and nitrates are less then 50 ppm */
-}else{
-	change = "You need to change the tank water"; /* Result if ammonia, nitrites don't equal 0 and nitrates are greater then 50 ppm */
-}
+/* How much money do I need to pay when I get to Pennsic War? */
 
-console.log(change);
+function pennsicFees(fee, preRegDiscount, memberFee){
+	var total =  fee - preRegDiscount - memberFee;
+	console.log("You need $" + total + " when you get to Pennsic War.");
+	
+};
+pennsicFees(165, 10, 0);
+
