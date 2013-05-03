6125cef2e01e0efd0c9e5dacda0cda609167bd23
tthorne <tthorne@fullsail.edu>
2013-05-03 00:44:16 -0300

    Update script.js

--- Assignment- Functions/Functions_Industry/js/script.js
+++ Assignment- Functions/Functions_Industry/js/script.js
@@ -3,12 +3,13 @@
  * Date: Wednesday May 1, 2013
  * Assignment:  Assignment- Functions (Industry)
  */
-var industry = alert ("Did the store make a profit last month compared to the previous month?"); /* States what the code is for */
-var previousMonth = prompt("What was previous month's profit?"); /* Asks for the previous month profit amount. */
-var lastMonth = prompt("What was last month's profit?"); /* Asks for last month's profit amount. */
 
-profit = (previousMonth < lastMonth) ? "The store made a profit compared to last month." : "The store didn't make a profit compared to last month.";
-/* If last month profit was greater than the previous month then the result will be "The store made a profit compared to last month." */
-/* If the profit from last month wasn't greater than the previous month then the result will be "The store didn't make a profit compared to last month." */
 
-console.log(profit); /* Prints out result. */
+/* How much money do I need to pay when I get to Pennsic War? */
+var calcAre = function(width, height){
+  var area = width * height;
+	return area;
+}
+
+var a = calcArea (20, 30);
+console.log(a);
