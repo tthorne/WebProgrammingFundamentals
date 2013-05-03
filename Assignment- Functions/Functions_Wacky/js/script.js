2b549e6d21d4e96bc1b6439500b7c877446fecc2
tthorne <tthorne@fullsail.edu>
2013-05-03 01:35:21 -0300

    Update script.js

--- Assignment- Functions/Functions_Wacky/js/script.js
+++ Assignment- Functions/Functions_Wacky/js/script.js
@@ -6,11 +6,12 @@
  
 /* Should I get a bigger fish tank? */
 function tank(g, gFish, stock){  // Parameters
-var gallons = g; // Defining g as gallons
+var gallons = g; // Defining g as gallons of the tank
 var gallonsPerFish = gFish; // Defining gFish as gallonsPerFish
-var fish = stock * gallonsPerFish; 
+var fish = stock * gallonsPerFish; // Taking stock and multiplying it by the gallons per fish
 
-numberFish = (fish <= gallons || gallonsPerFish <= gallons) ? "You have plenty of room" : "You need a bigger tank.";
+numberFish = (fish < gallons || gallonsPerFish < gallons) ? "You have plenty of room" : "You need a bigger tank.";
+// The result if the total of fish is less then gallons of the tank or if gallonsPerFish is less than the gallons of the tank will be "You have plenty of room", if either are greater then the results will be. "You need a bigger tank."
 
 console.log(numberFish);
 }
