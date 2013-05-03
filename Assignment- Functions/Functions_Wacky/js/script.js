0aa5468ff4405e3c512ae78b1ec9b9210165c3b7
tthorne <tthorne@fullsail.edu>
2013-05-03 01:42:09 -0300

    Update script.js

--- Assignment- Functions/Functions_Wacky/js/script.js
+++ Assignment- Functions/Functions_Wacky/js/script.js
@@ -10,10 +10,10 @@ var gallons = g; // Defining g as gallons of the tank
 var gallonsPerFish = gFish; // Defining gFish as gallonsPerFish
 var fish = stock * gallonsPerFish; // Taking stock and multiplying it by the gallons per fish
 
-numberFish = (fish < gallons || gallonsPerFish < gallons) ? "You have plenty of room" : "You need a bigger tank.";
+numberFish = (fish < gallons && gallonsPerFish < gallons) ? "You have plenty of room" : "You need a bigger tank.";
 // The result if the total of fish is less then gallons of the tank or if gallonsPerFish is less than the gallons of the tank will be "You have plenty of room", if either are greater then the results will be. "You need a bigger tank."
 
 console.log(numberFish);
 }
 
-tank(75, 10, 4); // The arguments for the parameters
+tank(75, 10, 2); // The arguments for the parameters
